9d2a6f65ee3e0442c950439303e13d49a01f0700
icvitkov <ivana.cvit@hotmail.com>
2023-07-31 20:55:55 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   router: {
-    base: '/icvitkov.github.io/cv/' // Replace 'my-cv' with your GitHub repository name
+    base: '/cv/' // Replace 'my-cv' with your GitHub repository name
   },
   devtools: { enabled: true }
 })
