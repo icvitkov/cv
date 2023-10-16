26530beaaef0df9653d4cb32f7501b1780f74054
icvitkov <ivana.cvit@hotmail.com>
2023-10-16 21:50:11 +0200

    add projects

---
+++ components/ProjectItem.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+defineProps<{
+	client: string
+	name: string
+	link?: string
+	year: string
+	techStack?: string[]
+}>()
+</script>
+
+<template>
+	<div class="project">
+		<div class="project-header">
+			<p>{{ client }}</p>
+			<span class="year">{{ year }}</span>
+		</div>
+		<h5 class="project-name">{{ name }}</h5>
+		<span class="project-link">{{ link ? link : "private repo" }}</span>
+		<div class="project-description">
+			<slot name="description"></slot>
+		</div>
+		<div class="tech-stack" v-if="techStack?.length">
+			<span class="tech-item" v-for="(item, index) in techStack" :key="index">{{
+				item
+			}}</span>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.project {
+	border: 1px solid grey;
+	border-radius: 5px;
+	padding: 24px;
+	background: rgba(20, 20, 27, 0.493);
+	-webkit-backdrop-filter: blur(5px);
+	backdrop-filter: blur(5px);
+	display: flex;
+	flex-direction: column;
+}
+.project-header {
+	display: flex;
+	gap: 24px;
+	justify-content: space-between;
+
+	& .year {
+		opacity: 0.5;
+	}
+}
+
+.project-link {
+	color: $color-mod-blue;
+}
+
+.project-name {
+	font-weight: 400;
+	margin-bottom: 12px;
+	margin-top: 24px;
+	color: $color-mod-magenta;
+}
+
+.project-description {
+	margin-top: 24px;
+	flex-grow: 1;
+}
+.tech-stack {
+	padding-top: 12px;
+	display: flex;
+	flex-wrap: wrap;
+	gap: 12px;
+
+	& .tech-item {
+		border: 1px solid;
+		border-radius: 2px;
+
+		padding-inline: 5px;
+
+		&:nth-child(3n + 1) {
+			border-color: $color-mod-blue;
+			color: $color-mod-blue;
+		}
+
+		&:nth-child(3n + 2) {
+			border-color: $color-mod-magenta;
+			color: $color-mod-magenta;
+		}
+
+		&:nth-child(3n) {
+			border-color: $color-soft-orange;
+			color: $color-soft-orange;
+		}
+	}
+}
+</style>
---
+++ components/Projects.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="grid-section projects" id="projects">
+		<div class="grid-container">
+			<h2 class="projects-title">projects</h2>
+			<div class="projects-list">
+				<ProjectItem
+					client="Barrage"
+					year="2023"
+					name="Nuxt Starter Kit"
+					:tech-stack="[
+						'nuxt',
+						'vite',
+						'typescript',
+						'html5',
+						'turborepo',
+						'strapi',
+						'pnpm',
+					]"
+				>
+					<template #description>
+						Generated significant time savings by implementing a frontend
+						monorepo structure, enhancing code sharing and reusability across
+						multiple projects. Implemented Strapi for dedicated content
+						management within our Nuxt applications.
+					</template>
+				</ProjectItem>
+
+				<ProjectItem
+					client="Barrage"
+					year="2022 - 2023"
+					name="hive"
+					link="https://www.hivecloud.com"
+					:tech-stack="['vue3', 'vite', 'typescript', 'html5', 'postCSS']"
+				>
+					<template #description>
+						Developed and maintained a web page, end-user application, and
+						backoffice system for efficient provisioning of virtual machines and
+						bare metal instances, utilizing a secure Single Sign-On (SSO)
+						approach.
+					</template>
+				</ProjectItem>
+
+				<ProjectItem
+					client="All4Labels"
+					year="2021 - 2022"
+					name="all4Labels"
+					link="https://www.roango.com"
+					:tech-stack="['vue3', 'vite', 'typescript', 'html5', 'sass']"
+				>
+					<template #description>
+						My primary role revolved around the end-user application, addressing
+						various user roles and their privileges. A notable achievement was
+						creating a flexible QR and barcode scanner, crucial to the project's
+						core aim: combining digitally printed serialized labels, optional
+						hardware, and a smart SaaS cloud.
+					</template>
+				</ProjectItem>
+
+				<ProjectItem
+					client="Roango"
+					year="2021"
+					name="Roango"
+					link="https://www.roango.com"
+					:tech-stack="['react', 'typescript', 'gatsby', 'html5', 'sass']"
+				>
+					<template #description>
+						I was maintaining both the web page and the backoffice application
+						for a project dedicated to transforming the recruitment approach for
+						companies and talents, facilitating a quicker and easier matching
+						process. The primary focus was on the backoffice application,
+						ensuring precise alignment of user roles with the appropriate
+						privileges to enable specific actions.
+					</template>
+				</ProjectItem>
+
+				<ProjectItem
+					client="Sartura"
+					year="2020 - 2021"
+					name="Kaon"
+					:tech-stack="['vue2', 'webpack', 'html5', 'quasar']"
+				>
+					<template #description>
+						Managed the user interface for OpenWrt-based devices designed for a
+						prominent Korean broadband service provider. My role primarily
+						involved maintaining the application and actively contributing to
+						the development of new features.
+					</template>
+				</ProjectItem>
+
+				<ProjectItem
+					client="Sartura"
+					year="2020 - 2021"
+					link="https://www.sentinel-sps.com/"
+					name="Sentinel"
+					:tech-stack="['vue2', 'webpack', 'html5', 'sass']"
+				>
+					<template #description>
+						Participated in a white-label project aimed at developing innovative
+						features for devices utilizing OpenWrt technology. The project's
+						primary objective was to provide images tailored to individual
+						clients. The most substantial challenge revolved around refactoring
+						the entire application while simultaneously ensuring the continued
+						operation of the older version in a live production environment.
+					</template>
+				</ProjectItem>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.projects-title {
+	grid-column: 1/-1;
+	justify-self: center;
+}
+
+.projects-list {
+	grid-column: 1/-1;
+	display: grid;
+	gap: 24px;
+
+	@media screen and (min-width: 768px) {
+		grid-template-columns: repeat(2, 1fr);
+	}
+
+	@media screen and (min-width: 1024px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+}
+
+.projects {
+	position: relative;
+	&::after {
+		content: "";
+		width: 30%;
+		height: 30%;
+		border-radius: 50%;
+		background: linear-gradient($color-light-blue 20%, $color-mod-magenta);
+		top: 45%;
+		left: 30%;
+		opacity: 0.2;
+		filter: blur(50px);
+		position: absolute;
+		box-shadow: -5px 80px 150px $color-mod-magenta,
+			-5px -80px 150px $color-light-blue;
+		z-index: -1;
+	}
+}
+</style>
